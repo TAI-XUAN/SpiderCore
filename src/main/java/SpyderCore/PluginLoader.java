c90b95252de15332d52655c01e74e45bee221bb1
Cuzz_0908 <ootmnt150oo@gmail.com>
2023-12-14 10:25:35 +0800

    :tada: 插件初始化

---
+++ src/main/java/SpyderCore/PluginLoader.java
@@ -0,0 +1,20 @@
+package SpyderCore;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class PluginLoader extends PluginBase {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
