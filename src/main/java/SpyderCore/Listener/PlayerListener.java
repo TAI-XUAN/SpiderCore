2b8a86b04d515fe76e448c354210e5ed0bc3eb91
Cuzz_0908 <ootmnt150oo@gmail.com>
2023-12-15 16:07:48 +0800

    :sparkles: 新增物品及座標檢義操作

--- src/main/java/SpyderCore/Listener/PlayerListener.java
+++ src/main/java/SpyderCore/Listener/PlayerListener.java
@@ -7,6 +7,8 @@ import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.inventory.InventoryPickupItemEvent;
 import cn.nukkit.event.player.PlayerLocallyInitializedEvent;
 import cn.nukkit.event.player.PlayerQuitEvent;
 
@@ -26,5 +28,14 @@ public class PlayerListener implements Listener {
     public void savePlayer(PlayerQuitEvent event){
         Player player = event.getPlayer();
         dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime()+"離開了伺服器");
+    }
+    @EventHandler
+    public void onPickUpItem(InventoryPickupItemEvent event){
+
+    }
+    @EventHandler
+    public void onBreak(BlockBreakEvent event){
+        Player player = event.getPlayer();
+        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime()+"破壞了"+event.getBlock().getName()+"在位置"+event.getBlock().getLocation().toString());
     }
 }
