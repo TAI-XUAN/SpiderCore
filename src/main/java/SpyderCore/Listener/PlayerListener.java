3c699f05d2a859dcc9921ba94d13518f8dd95a5e
fengning819 <50288269+fengning248@users.noreply.github.com>
2024-01-06 08:26:10 +0800

    Update PlayerListener.java
    ..就添加了玩家捡起物品的那个还有执行指令

--- src/main/java/SpyderCore/Listener/PlayerListener.java
+++ src/main/java/SpyderCore/Listener/PlayerListener.java
@@ -1,4 +1,4 @@
-package SpyderCore.Listener;
+packagepackage SpyderCore.Listener;
 
 import SpyderCore.DataLogSystem.DataLoggerManager;
 import SpyderCore.DataLogSystem.Types.MinecraftDataLog;
@@ -9,33 +9,47 @@ import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockBreakEvent;
 import cn.nukkit.event.inventory.InventoryPickupItemEvent;
+import cn.nukkit.event.player.PlayerCommandPreprocessEvent; 
 import cn.nukkit.event.player.PlayerLocallyInitializedEvent;
 import cn.nukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
     private MinecraftDataLog dataLog = (MinecraftDataLog) DataLoggerManager.getDataLogger("遊戲行為");
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void initPlayer(PlayerLocallyInitializedEvent event){
         Player player = event.getPlayer();
         DataLoggerManager.initPlayerLog(player.getUniqueId().toString());
     }
+
     @EventHandler
     public void loadPlayer(PlayerLocallyInitializedEvent event){
         Player player = event.getPlayer();
-        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime()+"登入了伺服器");
+        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime() + "登入了伺服器");
     }
+
     @EventHandler
     public void savePlayer(PlayerQuitEvent event){
         Player player = event.getPlayer();
-        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime()+"離開了伺服器");
+        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime() + "離開了伺服器");
     }
-    @EventHandler
-    public void onPickUpItem(InventoryPickupItemEvent event){
 
+    @EventHandler
+    public void onPickUpItem(InventoryPickupItemEvent event) {
+        Player player = (Player) event.getPlayer();
+        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime() + "撿起了物品：" + event.getItem().getName());
     }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime() + "執行了指令：" + event.getMessage());
+    }
+
     @EventHandler
     public void onBreak(BlockBreakEvent event){
         Player player = event.getPlayer();
-        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime()+"破壞了"+event.getBlock().getName()+"在位置"+event.getBlock().getLocation().toString());
+        dataLog.addPlayerData(player.getName(), PluginLoader.getLocalTime() + "破壞了" + event.getBlock().getName() +
+                "在位置" + event.getBlock().getLocation().toString());
     }
 }
